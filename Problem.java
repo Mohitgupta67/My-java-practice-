571d7e9a52b70ccee9641845114e1f2768c0335c
Mohitgupta67 <guptamht7275@gmail.com>
2026-08-17 04:45:11 +0530

    Create Problem.java Added program to find maximum of three numbers

---
+++ Problem.java
@@ -0,0 +1,8 @@
+public class Problem {
+    public static void main(String args[]) {
+        int a = 23;
+        int b = 25;
+        int c = 29;
+        System.out.println(Math.max(a,(Math.max(b,c))));
+    }
+}
